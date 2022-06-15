8595105b4aa63dc52e2ac9720884147912ad7cb0
BlueEyesssss <vuamessi9@gmail.com>
2022-06-15 11:55:04 +0700

    refactor the main code anh green 4ever always

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ### You will find in this repo the following stuff:
 * The Math-Util library that offers multiple math function (a Java-based source code)
 * Unit Testing source code with IUnit framework
+* GitHUb Actions to demonstrate the Continous Intergration (CI) technique
+* Regression Test
 * ...
 
 ### ScreenShrots
--- src/com/khaphp/mathutil/core/MathUtil.java
+++ src/com/khaphp/mathutil/core/MathUtil.java
@@ -23,18 +23,41 @@ public class MathUtil {
         //20 giai thừa 18 con số 0, vừa kịp đủ cho kiễu long của java
         //21 gia hừa tràn kiểu long
         //bài nay quy ước tính n! trong khoảng 1..20
+//        public static long getFactorial(int n){
+//            if(n < 0 || n > 20){
+//                throw new IllegalArgumentException("Invalid argument, N must be between 0..20");
+//            }
+//            
+//            if(n == 0 || n == 1)
+//                return 1;   //kết thúc cuộc chơi sớm nếu nhận đc giá trị đặc biệt
+//            
+//            long product = 1;   //tích  nhân dồn, thuật toán con heo đất, ốc bưu dồn thịt
+//            for (int i = 2; i <= n; i++)
+//                product *= i;
+//            
+//            return product;
+//        }
+        
         public static long getFactorial(int n){
             if(n < 0 || n > 20){
-                throw new IllegalArgumentException("Invalid argument, N must be between 0..20");
+                throw new IllegalArgumentException("n must be  between 0..20. PLz!");
             }
-            
             if(n == 0 || n == 1)
                 return 1;   //kết thúc cuộc chơi sớm nếu nhận đc giá trị đặc biệt
-            
-            long product = 1;   //tích  nhân dồn, thuật toán con heo đất, ốc bưu dồn thịt
-            for (int i = 2; i <= n; i++)
-                product *= i;
-            
-            return product;
+            return n * getFactorial(n-1);
         }
+        
+        //Học đệ quy trong vòng 30s
+        //đệ quy là gọi lại chính mình vs 1 quy mô khác
+        //vd: búp bê nga(búp to -> nhỏ hơn -> nhỏ hơn -> ...)
+        
+        //tính giùm tui 6!
+        //n! = 1.2.3.4.5. ... .n
+        //6! = 6 * 5!
+        //5! = 5 * 4!
+        //4! = 4 * 3!
+        //3! = 3 * 2!
+        //2! = 2 * 1!
+        //1! = 1    //quy ước 1! = 0! = 1
+        //chốt hả: n! = n * (n - 1)!
 }
